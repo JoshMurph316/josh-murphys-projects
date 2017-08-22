29064031716743ea81d9929bfb486e91b6bfc796
Joshmurph316 <Joshmurph316@gmail.com>
2017-08-22 17:56:49 -0400

    added some edits

---
+++ public/javascripts/wiki-view.js
@@ -0,0 +1,8 @@
+var entry = document.getElementById('search').value;
+
+var submit = document.getElementById('submit');
+
+
+submit.addEventListener('click', function() {
+    console.log(entry);
+});
--- routes/projects.js
+++ routes/projects.js
@@ -1,28 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/city-search', function(req, res, next) {
-    res.render('city-search');
-});
-
-router.get('/array-work', function(req, res, next) {
-    res.render('array-work');
-});
-router.get('/array-work2', function(req, res, next) {
-    res.render('array-work2');
-});
-router.get('/css-variables', function(req, res, next) {
-    res.render('css-variables');
-});
-router.get('/flex-panels', function(req, res, next) {
-    res.render('flex-panels');
-});
-router.get('/drum-kit', function(req, res, next) {
-    res.render('drum-kit');
-});
-router.get('/js-clock', function(req, res, next) {
-    res.render('js-clock');
+router.get('/:id', function(req, res, next) {
+    res.render(req.params.id);
 });
 
 module.exports = router;
