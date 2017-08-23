c42f1382243fd2292566c96afedd8a93f754205e
Joshmurph316 <Joshmurph316@gmail.com>
2017-08-23 12:35:23 -0400

    search

--- public/javascripts/wiki-view.js
+++ public/javascripts/wiki-view.js
@@ -1,8 +1,21 @@
+var endpoint = 'https://en.wikipedia.org/wiki/Special:Random';
+
 var entry = document.getElementById('search').value;
 
 var submit = document.getElementById('submit');
 
+function getWiki() {
+    var http = new XMLHttpRequest();
 
-submit.addEventListener('click', function() {
-    console.log(entry);
-});
+    http.onreadystatechange = () => {
+
+        if (http.readyState == 4 && http.status == 200) {
+            let result = JSON.parse(http.response);
+            console.log(result);
+        }
+    };
+    http.open("GET", endpoint, true);
+    http.send();
+};
+
+submit.addEventListener('click', getWiki);
